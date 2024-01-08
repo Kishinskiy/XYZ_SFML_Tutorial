8465a38e3ab549b3ce1cda124d32e0bfaa66fbef
Oleg <oleg@MacBook-Pro-user.local>
2024-01-08 18:42:40 +0300

    hotfix

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const float INITITAL_SPEED = 100.f;
 const float PLAYER_SIZE = 20.f;
-const float ACCELERATION = 100.f;
+const float ACCELERATION = 20.f;
 const int NUM_APPLES = 20;
 const float APPLE_SIZE = 20.f;
 
